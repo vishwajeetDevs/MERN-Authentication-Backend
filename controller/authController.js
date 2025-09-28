08db8d6f906e6d9c57e441f2633e91fd9d03464e
vishwajeetDevs <justvishu2003@gmail.com>
2025-09-28 15:48:00 +0530

    .

--- controller/authController.js
+++ controller/authController.js
@@ -29,7 +29,8 @@ export const register = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            // sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            sameSite: process.env.NODE_ENV === "none",
             maxAge: 7*24*60*60*1000
         }); 
 
@@ -70,7 +71,8 @@ export const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            // sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            sameSite: process.env.NODE_ENV ==="none",
             maxAge: 7*24*60*60*1000
         });
 
